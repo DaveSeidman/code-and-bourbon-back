43b1090b445acd98ec323f6e255e27375c72f4ec
Dave Seidman <daveseidman@gmail.com>
2025-12-01 08:33:52 -0500

    fix for safari

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: false,
+  saveUninitialized: true,
   store: MongoStore.create({
     mongoUrl: process.env.MONGO_URI,
     collectionName: 'sessions',
